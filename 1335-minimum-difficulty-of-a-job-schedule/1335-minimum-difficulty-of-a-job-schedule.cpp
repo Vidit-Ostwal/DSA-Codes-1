d2b6a5c099dc615e90f9fc285c2237532189ee43
Vidit Ostwal <110953813+Vidit-Ostwal@users.noreply.github.com>
2022-10-16 13:19:36 +0530

    Time: 180 ms (25.48%), Space: 400.2 MB (5.09%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,32 +1,34 @@
-class Solution {
-  vector<vector<vector<long long>>> DP;
-  long long Recursive_Solution(vector<int>& A, int index, int curr_maxi, int part)
-  {
-    if (part < 0)
-      return INT_MAX;
-    
-    if (index == A.size())
+class Solution
+{
+    vector<vector<vector< long long>>> DP;
+    long long Recursive_Solution(vector<int> &A, int index, int curr_maxi, int part)
     {
-      if (part || curr_maxi == INT_MIN)
-        return INT_MAX;
-      else
-        return curr_maxi;
+        if (part < 0)
+            return INT_MAX;
+
+        if (index == A.size())
+        {
+            if (part || curr_maxi == INT_MIN)
+                return INT_MAX;
+            else
+                return curr_maxi;
+        }
+
+        int temp = (curr_maxi == INT_MIN) ? 1001 : curr_maxi;
+
+        if (DP[index][part][temp] != -1)
+            return DP[index][part][temp];
+
+        curr_maxi = max(curr_maxi, A[index]);
+        return DP[index][part][temp] = min(Recursive_Solution(A, index + 1, curr_maxi, part), curr_maxi + Recursive_Solution(A, index + 1, INT_MIN, part - 1));
     }
-    
-    int temp = (curr_maxi == INT_MIN) ? 1001 : curr_maxi;
-    
-    if (DP[index][part][temp] != -1)
-      return DP[index][part][temp];
-    
-    curr_maxi = max(curr_maxi,A[index]);
-    return DP[index][part][temp] = min(Recursive_Solution(A, index+1, curr_maxi,part), curr_maxi + Recursive_Solution(A, index+1,INT_MIN,part-1)); 
-  }
-public:
-    int minDifficulty(vector<int>& A, int d) {
-      DP = vector<vector<vector<long long>>> (A.size(),vector<vector<long long>>(11,vector<long long>(1002,-1)));
-      if (d > A.size())
-          return -1;
-      
-      return Recursive_Solution(A,0,INT_MIN,d-1);
-    }
+    public:
+        int minDifficulty(vector<int> &A, int d)
+        {
+            DP = vector<vector<vector< long long>>> (A.size(), vector<vector < long long>> (11, vector < long long > (1002, -1)));
+            if (d > A.size())
+                return -1;
+
+            return Recursive_Solution(A, 0, INT_MIN, d - 1);
+        }
 };
